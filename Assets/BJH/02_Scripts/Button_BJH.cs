bb35ab6ce5bb8812a4e5145e7fd7acd52b5b98c1
Jihwan Byeon <wkdghk24@gmail.com>
2024-07-26 16:22:48 +0900

    feat : 인벤토리 아웃라인
    - 선택된 인벤토리는 아웃라인 활성화
    - OnClick()을 assign으로 적용했음. 필요시 코드로 변경 예정

--- Assets/BJH/02_Scripts/Button_BJH.cs
+++ Assets/BJH/02_Scripts/Button_BJH.cs
@@ -25,4 +25,29 @@ public class Button_BJH : Button
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(2);
     }
+
+    // 선택되면 selector 켜고 끄기
+    // 그런데 다른 오브젝트가 선택된다면?
+    [SerializeField] private GameObject selectedWeaponSelector;
+    public void OnClickSelector(GameObject selector)
+    {
+        // 이미 선택된 셀렉터가 있다면?
+        if (selectedWeaponSelector != null)
+        {
+            // 이미 선택된 셀렉터가 자기 자신이라면?
+            if (selectedWeaponSelector.name == selector.name)
+            {
+                selectedWeaponSelector.GetComponent<Image>().enabled = false;
+                selectedWeaponSelector = null;
+                return;
+            }
+            else
+            {
+                // 선택된 셀렉터 비활성화
+                selectedWeaponSelector.GetComponent<Image>().enabled = false;
+            }
+        }
+        selector.GetComponent<Image>().enabled = true;
+        selectedWeaponSelector = selector;
+    }
 }
--- Assets/BJH/02_Scripts/InventoryItems.cs
+++ Assets/BJH/02_Scripts/InventoryItems.cs
@@ -5,8 +5,17 @@ using UnityEngine.UI;
 
 public class InventoryItems : MonoBehaviour
 {
+    public List<Image> inventoryItemSelectors = new List<Image>();
     public List<Image> inventoryItemImages = new List<Image> ();
     private int index = 0;
+
+    private void Start()
+    {
+        foreach (var selector in inventoryItemSelectors)
+        {
+            selector.enabled = false;
+        }
+    }
 
     public void UpdateInventoryItemImages(Sprite sprite)
     {
