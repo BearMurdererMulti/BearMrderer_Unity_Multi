7c2729cc5c219c9226b5e1681f98b6f00793ad8d
Jihwan Byeon <wkdghk24@gmail.com>
2024-07-30 23:49:13 +0900

    feat : 시네머신 1차 완성
    - 시네머신이 종료되면 스크립트를 실행하고 다음 시네머신을 실행하는 방법을 모색해야 나아갈 수 있음

--- Assets/BJH/02_Scripts/CinemachineManager.cs
+++ Assets/BJH/02_Scripts/CinemachineManager.cs
@@ -1,21 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
 
 public class CinemachineManager : MonoBehaviour
 {
-    [SerializeField] GameObject doll, dog;
-    [SerializeField] float setSpawnTime;
+    [SerializeField] private GameObject doll, dog;
+    [SerializeField] private float setSpawnTime;
 
-    [SerializeField] List<Animator> trafficLightAnimators;
-    [SerializeField] float setTrafficLightTime;
+    [SerializeField] private List<Animator> trafficLightAnimators;
+    [SerializeField] private float setTrafficLightTime;
 
+    [SerializeField] private GameObject talkUi;
+    [SerializeField] private TMP_Text talkContent;
+    [SerializeField] private List<string> talkList;
+    private int talkIndex = 0;
+    private bool isTalking = false;
+
+    [SerializeField] private PlayableDirector initialTimeline;
 
     private void Start()
     {
+        // 타임라인에서 타이밍 맞게 시작, 끝내야 할 것들을 코루틴으로 구현
         StartCoroutine(CoSpawnDollAndDog());
         StartCoroutine(CoTurnOffTrafficLight());
+
+        // 이벤트 구독
+
     }
+
+    private void StartInitialTimeLine()
+    {
+        //initialTimeline.played += O
+    }
+
+    private void Update()
+    {
+        if(talkUi.activeSelf && Input.GetKeyDown(KeyCode.KeypadEnter) && Input.GetKeyDown(KeyCode.K))
+        {
+            ShowNextTalkScript();
+        }
+    }
+
 
     IEnumerator CoSpawnDollAndDog()
     {
@@ -33,5 +63,17 @@ public class CinemachineManager : MonoBehaviour
         }
     }
 
-
+    
+    public void StartTalking()
+    {
+        isTalking = true;
+        talkUi.SetActive(true);
+        talkContent.text = talkList[talkIndex];
+        talkIndex++;
+    }
+    private void ShowNextTalkScript()
+    {
+        talkContent.text = talkList[talkIndex];
+        talkIndex++;
+    }
 }
