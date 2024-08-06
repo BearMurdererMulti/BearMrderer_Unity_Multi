cb904fbdd51ae8e24140ddae828f620b825bcfcf
juyeon0514 <sk651726@gmail.com>
2024-08-06 17:26:53 +0900

    진짜바꿈
    했음

--- Assets/juykang/2.KJY_Script/KJY_PhotonConnection.cs
+++ Assets/juykang/2.KJY_Script/KJY_PhotonConnection.cs
@@ -31,7 +31,7 @@ public class KJY_PhotonConnection : MonoBehaviourPunCallbacks
             {
                 InfoManagerKJY.instance.npcOxDic.Add(npc.npcName.ToString(), null);
             }
-            string sceneName = SceneName.GameScene_NPC_Random.ToString();
+            string sceneName = SceneName.Chinemachine_01.ToString();
             PhotonNetwork.LoadLevel(sceneName);
         }
     }
