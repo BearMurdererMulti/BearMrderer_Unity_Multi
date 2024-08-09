e7cc7757719c4cb9973b7bcd8df5a0ed70edbc0c
Jihwan Byeon <wkdghk24@gmail.com>
2024-08-10 02:04:54 +0900

    feat : 인벤토리 동기화 최종 완성

--- Assets/BJH/02_Scripts/DogCanvas.cs
+++ Assets/BJH/02_Scripts/DogCanvas.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,24 @@ using UnityEngine;
 public class DogCanvas : MonoBehaviour
 {
     [SerializeField] private GameObject inventoryImage;
-    [SerializeField] private InventoryItems inventoryItems;
+    [SerializeField] private InventoryItems dollInventoryItems;
+    [SerializeField] private InventoryItems dogInventoryItems;
+
 
     private void Start()
     {
         GameObject weaponManagerGameobject = GameObject.Find("WeaponManager");
-        weaponManagerGameobject.GetComponent<WeaponManager>().inventoryItems = inventoryItems; 
-        Debug.Log(weaponManagerGameobject.GetComponent<WeaponManager>().inventoryItems.ToString() + "¾î½ÎÀÎ Àß µÆ³Ä?");
-        inventoryImage.SetActive(false);
+
+        if(PhotonNetwork.IsMasterClient)
+        {
+            weaponManagerGameobject.GetComponent<WeaponManager>().inventoryItems = dollInventoryItems;
+            inventoryImage.SetActive(false);
+        }
+        else
+        {
+            weaponManagerGameobject.GetComponent<WeaponManager>().inventoryItems = dogInventoryItems; 
+            inventoryImage.SetActive(false);
+
+        }
     }
 }
